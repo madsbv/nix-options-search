182985ce7ab4c82edba211af84391f47d758ab2d
Mads Bach Villadsen <mvilladsen@pm.me>
2024-03-01 17:52:14 +0100

    Add CLI based search loop

--- src/main.rs
+++ src/main.rs
@@ -8,71 +8,67 @@
 )]
 
 use anyhow::Result;
-use nucleo::pattern::{Atom, AtomKind, CaseMatching, Normalization};
-use nucleo::Config;
+use nucleo::pattern::{CaseMatching, Normalization};
+use nucleo::{Config, Nucleo};
 
 mod opt_data;
-use opt_data::{parse_options, OptData};
+use opt_data::parse_options;
 
 fn main() -> Result<()> {
     // let body: String = ureq::get("https://daiderd.com/nix-darwin/manual/index.html")
     //     .call()?
     //     .into_string()?;
-    // let body = std::fs::read_to_string("data/index.html").unwrap();
-    let body = std::fs::read_to_string("data/index-short.html").unwrap();
+    let body = std::fs::read_to_string("data/index.html").unwrap();
+    // let body = std::fs::read_to_string("data/index-short.html").unwrap();
     let dom = tl::parse(&body, tl::ParserOptions::default())?;
-    let opts = parse_options(&dom);
+    let opt_strings = parse_options(&dom).into_iter().map(|o| o.to_string());
 
-    let opt_strings = opts.iter().map(OptData::to_string);
-
-    let pattern = "documentation";
-
-    let mut nuc =
-        nucleo::Nucleo::<String>::new(Config::DEFAULT, std::sync::Arc::new(|| ()), None, 1);
+    let mut nuc = create_nuc(1);
     let inj = nuc.injector();
     for s in opt_strings {
+        // NOTE: First argument is the "data" part of matched items; use it to store the data you want to get out at the end (e.g. the entire object you're searching for, or an index to it).
+        // The second argument is a closure that outputs the text that should be displayed as the user, and which Nucleo matches a given pattern against. For us, that could be the contents of the various fields of OptData in different columns
         inj.push(s.clone(), |fill| fill[0] = s.into());
     }
-    eprintln!("{}", inj.injected_items());
-
-    nuc.pattern
-        .reparse(0, pattern, CaseMatching::Smart, Normalization::Smart, false);
-
     nuc.tick(10);
 
-    let snap = nuc.snapshot();
-    let n = snap.matched_item_count();
-    eprintln!("{n}");
-    for m in snap.matched_items(0..n) {
-        eprintln!("{}", m.data);
+    let mut buffer = String::with_capacity(32);
+    loop {
+        buffer.clear();
+        println!("Enter search query:");
+        std::io::stdin().read_line(&mut buffer)?;
+        nuc.pattern.reparse(
+            0,
+            &buffer,
+            CaseMatching::Ignore,
+            Normalization::Smart,
+            false,
+        );
+
+        // NOTE: tick(n) waits up to n ms for the matcher to finish, then returns. Here, we'd rather wait longer for it to really finish than risk it outputting the wrong thing.
+        if nuc.tick(2000).running {
+            println!("Searching is taking unusually long");
+            while nuc.tick(1000).running {
+                println!("working...");
+            }
+        }
+
+        let snap = nuc.snapshot();
+        let n = snap.matched_item_count();
+        // NOTE: Can do `n.min(cap)` to output at most `cap` search results.
+        for m in snap.matched_items(0..n).rev() {
+            println!("{}", m.data);
+        }
+        eprintln!("You searched for: {buffer}");
+        eprintln!("{n}");
     }
-    // let matches = fuzzy_match(pattern, opt_strings);
-
-    // for m in matches.iter().take(10) {
-    //     eprintln!("{} \n Score: {}\n", m.0, m.1);
-    // }
-
-    Ok(())
 }
 
-/// convenience function to easily fuzzy match
-/// on a (relatively small list of inputs). This is not recommended for building a full tui
-/// application that can match large numbers of matches as all matching is done on the current
-/// thread, effectively blocking the UI
-// Taken from https://github.com/helix-editor/helix/blob/d0bb77447138f5f70f96b174a8f29045a956c8c4/helix-core/src/fuzzy.rs#L4
-// Look into optimizations/keeping a global matcher around as a static, like helix does
-pub fn fuzzy_match<T: AsRef<str>>(
-    pattern: &str,
-    items: impl IntoIterator<Item = T>,
-) -> Vec<(T, u16)> {
-    let mut matcher = nucleo::Matcher::default();
-    matcher.config = Config::DEFAULT;
-    let pattern = Atom::new(
-        pattern,
-        CaseMatching::Smart,
-        Normalization::Smart,
-        AtomKind::Fuzzy,
-        false,
-    );
-    pattern.match_list(items, &mut matcher)
+fn create_nuc(columns: u32) -> Nucleo<String> {
+    Nucleo::<String>::new(
+        Config::DEFAULT,
+        std::sync::Arc::new(|| ()),
+        Some(1),
+        columns,
+    )
 }
