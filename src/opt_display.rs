b32f2e70b82ef7a3c58c1e95ac529e3b3bd2a97d
Mads Bach Villadsen <mvilladsen@pm.me>
2024-04-27 14:07:57 +0200

    Replace custom text wrapping with Ratatui's builtins

--- src/opt_display.rs
+++ src/opt_display.rs
@@ -1,8 +1,8 @@
-use std::borrow::{BorrowMut, Cow};
-
 use crate::opt_data::OptText;
-use ratatui::{prelude::*, widgets::Paragraph};
-use textwrap::{wrap, Options};
+use ratatui::{
+    prelude::*,
+    widgets::{Paragraph, Wrap},
+};
 
 /// A widget to display a single option parsed from nix-darwin/nixos/home-manager.
 /// Layout:
@@ -34,23 +34,16 @@ impl Widget for &OptText {
 
         let title_style = Style::new().blue();
 
-        let name = wrapped_paragraph_with_title(&self.name, "Name: ", title_style, inner_top[0]);
-        let var_type =
-            wrapped_paragraph_with_title(&self.var_type, "Type: ", title_style, inner_top[1]);
-        let default =
-            wrapped_paragraph_with_title(&self.default, "Default: ", title_style, inner_top[2]);
+        let name = wrapped_paragraph_with_title(&self.name, "Name: ", title_style);
+        let var_type = wrapped_paragraph_with_title(&self.var_type, "Type: ", title_style);
+        let default = wrapped_paragraph_with_title(&self.default, "Default: ", title_style);
         name.render(inner_top[0], buf);
         var_type.render(inner_top[1], buf);
         default.render(inner_top[2], buf);
 
-        let description = wrapped_paragraph_with_title(
-            &self.description,
-            "Description: ",
-            title_style,
-            inner_bottom[0],
-        );
-        let example =
-            wrapped_paragraph_with_title(&self.example, "Example: ", title_style, inner_bottom[1]);
+        let description =
+            wrapped_paragraph_with_title(&self.description, "Description: ", title_style);
+        let example = wrapped_paragraph_with_title(&self.example, "Example: ", title_style);
         description.render(inner_bottom[0], buf);
         example.render(inner_bottom[1], buf);
     }
@@ -60,64 +53,13 @@ fn wrapped_paragraph_with_title<'a>(
     content: &'a str,
     title: &'a str,
     title_style: Style,
-    area: Rect,
 ) -> Paragraph<'a> {
     let title_span = Span::styled(title, title_style);
 
-    if area.height <= 1 {
-        // Avoid running text wrapping algorithm if not needed.
-        return Paragraph::new(Line::from(vec![title_span, content.to_string().into()]));
-    }
-
-    let mut wrapped = wrap_text(content, title, area.width as usize);
-    let mut lines = vec![];
-
-    lines.push(Line::from(vec![
-        title_span,
-        std::mem::take(&mut wrapped[0]).into(),
-    ]));
-
-    for w in wrapped.into_iter().skip(1).take(area.height as usize - 1) {
-        lines.push(Line::from(w.to_string()));
-    }
-    Paragraph::new(Text::from(lines))
-}
-
-fn wrap_text<'a>(content: &'a str, title: &'a str, width: usize) -> Vec<Cow<'a, str>> {
-    let options = Options::new(width).initial_indent(title);
-
-    let mut wrapped = wrap(content, options);
-
-    wrapped[0] = Cow::Owned(
-        wrapped[0]
-            .borrow_mut()
-            .strip_prefix(title)
-            .expect("wrapping with initial_indent `title` prefixes `wrapped[0]` with `title`")
-            .into(),
-    );
-    wrapped
+    return Paragraph::new(Line::from(vec![title_span, content.to_string().into()]))
+        .wrap(Wrap { trim: true });
 }
 
 impl OptText {
     pub const HEIGHT: usize = 3;
-}
-
-#[cfg(test)]
-mod tests {
-    
-
-    use super::*;
-
-    #[test]
-    fn test_wrapping() {
-        let title = "Test: ";
-        let content = "This is a string of length 74. The text to be wrapped has total length 80.";
-        let wrapped = wrap_text(content, title, 40);
-
-        assert_eq!(wrapped[0].as_ref(), "This is a string of length 74. The");
-        assert_eq!(
-            wrapped[1].as_ref(),
-            "text to be wrapped has total length 80."
-        );
-    }
 }
