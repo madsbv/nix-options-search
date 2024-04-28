8b3a229626d01870ce2ed28dcf87d8e5b9a17193
Mads Bach Villadsen <mvilladsen@pm.me>
2024-04-28 22:21:27 +0200

    Make highlighted item taller, put rendering logic into OptDisplay

--- src/opt_display.rs
+++ src/opt_display.rs
@@ -13,12 +13,56 @@ use tui_widget_list::{ListableWidget, ScrollAxis};
 /// #     ........................              ........ #
 /// ######################################################
 ///
-/// This widget only handles text layout and has no border built in.
-impl Widget for &OptText {
+
+fn wrapped_paragraph_with_title<'a>(
+    content: &'a str,
+    title: &'a str,
+    title_style: Style,
+) -> Paragraph<'a> {
+    let title_span = Span::styled(title, title_style);
+
+    return Paragraph::new(Line::from(vec![title_span, content.to_string().into()]))
+        .wrap(Wrap { trim: true });
+}
+
+pub struct ListableOptWidget {
+    content: OptText,
+    height: usize,
+    style: Style,
+}
+
+// TODO: It might be worth making the height of highlighted items dynamic depending on the amount of text
+impl ListableOptWidget {
+    const DEFAULT_HEIGHT: usize = 4;
+    const HIGHLIGHTED_HEIGHT: usize = 7;
+}
+
+impl From<OptText> for ListableOptWidget {
+    fn from(value: OptText) -> Self {
+        ListableOptWidget {
+            content: value,
+            height: ListableOptWidget::DEFAULT_HEIGHT,
+            style: Style::default(),
+        }
+    }
+}
+
+impl From<ListableOptWidget> for OptText {
+    fn from(value: ListableOptWidget) -> Self {
+        value.content
+    }
+}
+
+impl Widget for ListableOptWidget {
     fn render(self, area: Rect, buf: &mut Buffer)
     where
         Self: Sized,
     {
+        let block = Block::default().style(self.style);
+        let inner = block.inner(area);
+        block.render(area, buf);
+
+        let area = inner;
         let outer = Layout::default()
             .direction(Direction::Vertical)
             .constraints([
@@ -40,73 +84,26 @@ impl Widget for &OptText {
 
         let title_style = Style::new().blue();
 
-        let name = wrapped_paragraph_with_title(&self.name, "Name: ", title_style);
-        let var_type = wrapped_paragraph_with_title(&self.var_type, "Type: ", title_style);
-        let default = wrapped_paragraph_with_title(&self.default, "Default: ", title_style);
+        // TODO: Compute the estimated max height of this widget given text wrapping.
+        // We'll have to do this manually with string lengths and such
+
+        let name = wrapped_paragraph_with_title(&self.content.name, "Name: ", title_style);
+        let var_type = wrapped_paragraph_with_title(&self.content.var_type, "Type: ", title_style);
+        let default = wrapped_paragraph_with_title(&self.content.default, "Default: ", title_style);
         name.render(inner_top[0], buf);
         var_type.render(inner_top[1], buf);
         default.render(inner_top[2], buf);
 
         let description =
-            wrapped_paragraph_with_title(&self.description, "Description: ", title_style);
-        let example = wrapped_paragraph_with_title(&self.example, "Example: ", title_style);
+            wrapped_paragraph_with_title(&self.content.description, "Description: ", title_style);
+        let example = wrapped_paragraph_with_title(&self.content.example, "Example: ", title_style);
         description.render(inner_bottom[0], buf);
         example.render(inner_bottom[1], buf);
     }
 }
 
-fn wrapped_paragraph_with_title<'a>(
-    content: &'a str,
-    title: &'a str,
-    title_style: Style,
-) -> Paragraph<'a> {
-    let title_span = Span::styled(title, title_style);
-
-    return Paragraph::new(Line::from(vec![title_span, content.to_string().into()]))
-        .wrap(Wrap { trim: true });
-}
-
-// TODO: Add some styling information for highlighted items
-pub struct ListableOptWidget {
-    content: OptText,
-    height: usize,
-    style: Style,
-}
-
-// TODO: It might be worth making the height of highlighted items dynamic depending on the amount of text
-impl ListableOptWidget {
-    pub const HEIGHT: usize = 4;
-    pub const HIGHLIGHTED_HEIGHT: usize = 7;
-}
-
-impl From<OptText> for ListableOptWidget {
-    fn from(value: OptText) -> Self {
-        ListableOptWidget {
-            content: value,
-            height: ListableOptWidget::HEIGHT,
-            style: Style::default(),
-        }
-    }
-}
-
-impl From<ListableOptWidget> for OptText {
-    fn from(value: ListableOptWidget) -> Self {
-        value.content
-    }
-}
-
-impl Widget for ListableOptWidget {
-    fn render(self, area: Rect, buf: &mut Buffer)
-    where
-        Self: Sized,
-    {
-        let block = Block::default().style(self.style);
-        let inner = block.inner(area);
-        block.render(area, buf);
-        OptText::from(self).render(inner, buf);
-    }
-}
-
+// TODO: Dynamic expanded height computation.
+// I think we need to move the rendering logic from OptText to ListableOptWidget, and store the desired height of a fully expanded item as state on the widget after doing the text wrapping. Then we need to reference that here.
 impl ListableWidget for ListableOptWidget {
     fn highlight(self) -> Self
     where
