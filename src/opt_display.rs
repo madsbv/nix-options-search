ce931282e093adc39a6cbad221772dad49d84079
Mads Bach Villadsen <mvilladsen@pm.me>
2024-04-29 13:20:39 +0200

    Stop wrapping some text, remove wrapping function

--- src/opt_display.rs
+++ src/opt_display.rs
@@ -1,7 +1,7 @@
 use crate::opt_data::OptText;
 use ratatui::{
     prelude::*,
-    widgets::{Block, Paragraph, Wrap},
+    widgets::{Block, Padding, Paragraph, Wrap},
 };
 use tracing::debug;
 use tui_widget_list::{ListableWidget, ScrollAxis};
@@ -13,18 +13,6 @@ use tui_widget_list::{ListableWidget, ScrollAxis};
 /// # Description: ...............          Example: ... #
 /// #     ........................              ........ #
 /// ######################################################
-///
-
-fn wrapped_paragraph_with_title<'a>(
-    content: &'a str,
-    title: &'a str,
-    title_style: Style,
-) -> Paragraph<'a> {
-    let title_span = Span::styled(title, title_style);
-
-    return Paragraph::new(Line::from(vec![title_span, content.to_string().into()]))
-        .wrap(Wrap { trim: true });
-}
 
 pub struct ListableOptWidget {
     content: OptText,
@@ -50,31 +38,46 @@ impl ListableOptWidget {
     }
 }
 
-impl From<ListableOptWidget> for OptText {
-    fn from(value: ListableOptWidget) -> Self {
-        value.content
-    }
-}
-
 impl Widget for ListableOptWidget {
     fn render(self, area: Rect, buf: &mut Buffer)
     where
         Self: Sized,
     {
-        let block = Block::default().style(self.style);
+        let title_style = Style::new().blue();
+
+        let name = Paragraph::new(Line::from(vec![
+            Span::styled("Name: ", title_style),
+            self.content.name.to_string().into(),
+        ]));
+        let var_type = Paragraph::new(Line::from(vec![
+            Span::styled("Type: ", title_style),
+            self.content.var_type.to_string().into(),
+        ]));
+        let default = Paragraph::new(Line::from(vec![
+            Span::styled("Default: ", title_style),
+            self.content.default.to_string().into(),
+        ]));
+        let description = Paragraph::new(Line::from(vec![
+            Span::styled("Description: ", title_style),
+            self.content.description.to_string().into(),
+        ]))
+        .wrap(Wrap { trim: true });
+        let example = Paragraph::new(Line::from(vec![
+            Span::styled("Example: ", title_style),
+            self.content.example.to_string().into(),
+        ]))
+        .wrap(Wrap { trim: true });
+
+        let block = Block::default()
+            .style(self.style)
+            .padding(Padding::bottom(1));
         let inner = block.inner(area);
         block.render(area, buf);
 
-        let area = inner;
         let outer = Layout::default()
             .direction(Direction::Vertical)
-            .constraints([
-                Constraint::Length(1),
-                Constraint::Percentage(100),
-                // Padding between list elements
-                Constraint::Length(1),
-            ])
-            .split(area);
+            .constraints([Constraint::Length(1), Constraint::Percentage(100)])
+            .split(inner);
 
         let inner_top = Layout::default()
             .direction(Direction::Horizontal)
@@ -85,18 +88,10 @@ impl Widget for ListableOptWidget {
             .constraints([Constraint::Ratio(2, 3), Constraint::Ratio(1, 3)])
             .split(outer[1]);
 
-        let title_style = Style::new().blue();
-
-        let name = wrapped_paragraph_with_title(&self.content.name, "Name: ", title_style);
-        let var_type = wrapped_paragraph_with_title(&self.content.var_type, "Type: ", title_style);
-        let default = wrapped_paragraph_with_title(&self.content.default, "Default: ", title_style);
         name.render(inner_top[0], buf);
         var_type.render(inner_top[1], buf);
         default.render(inner_top[2], buf);
 
-        let description =
-            wrapped_paragraph_with_title(&self.content.description, "Description: ", title_style);
-        let example = wrapped_paragraph_with_title(&self.content.example, "Example: ", title_style);
         description.render(inner_bottom[0], buf);
         example.render(inner_bottom[1], buf);
     }
